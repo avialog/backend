4e736dce846adcac354b933e7b1d7d1618bbc6e0
unknown <krzysiu.czechowicz@gmail.com>
2024-03-30 21:02:30 +0100

    add contact controller and tests for it

---
+++ internal/dto/errors.go
@@ -0,0 +1,9 @@
+package dto
+
+import "errors"
+
+var (
+	ErrNotFound        = errors.New("not found")
+	ErrInternalFailure = errors.New("internal failure")
+	ErrBadRequest      = errors.New("bad request")
+)
