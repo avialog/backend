a31ddd8ad6078159d44f77235f9b0ca989e37812
unknown <krzysiu.czechowicz@gmail.com>
2024-04-14 22:47:32 +0200

    update responses

--- internal/dto/aircraft_response.go
+++ internal/dto/aircraft_response.go
@@ -1,6 +1,7 @@
 package dto
 
 type AircraftResponse struct {
+	ID                 uint    `json:"id"`
 	RegistrationNumber string  `json:"registration_number" binding:"required"`
 	AircraftModel      string  `json:"aircraft_model" binding:"required"`
 	Remarks            *string `json:"remarks"`
