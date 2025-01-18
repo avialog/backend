a38faf9e918090a1da33ef38e8731571dda8ff63
unknown <krzysiu.czechowicz@gmail.com>
2025-01-18 23:30:13 +0100

    fix

--- internal/dto/user_request.go
+++ internal/dto/user_request.go
@@ -3,12 +3,13 @@ package dto
 import "github.com/avialog/backend/internal/model"
 
 type UserRequest struct {
-	FirstName    *string        `json:"first_name"`
-	LastName     *string        `json:"last_name"`
-	AvatarURL    *string        `json:"avatar_url"`
-	SignatureURL *string        `json:"signature_url"`
-	Country      *model.Country `json:"country"`
-	Phone        *string        `json:"phone"`
-	Address      *string        `json:"address"`
-	Timezone     *string        `json:"timezone"`
+	FirstName     *string        `json:"first_name"`
+	LastName      *string        `json:"last_name"`
+	AvatarURL     *string        `json:"avatar_url"`
+	SignatureURL  *string        `json:"signature_url"`
+	Country       *model.Country `json:"country"`
+	Phone         *string        `json:"phone"`
+	Address       *string        `json:"address"`
+	LicenseNumber *string        `json:"license_number"`
+	Timezone      *string        `json:"timezone"`
 }
--- internal/dto/user_response.go
+++ internal/dto/user_response.go
@@ -3,13 +3,14 @@ package dto
 import "github.com/avialog/backend/internal/model"
 
 type UserResponse struct {
-	FirstName    *string        `json:"first_name"`
-	LastName     *string        `json:"last_name"`
-	Email        string         `json:"email" binding:"required"`
-	AvatarURL    *string        `json:"avatar_url"`
-	SignatureURL *string        `json:"signature_url"`
-	Country      *model.Country `json:"country"`
-	Phone        *string        `json:"phone"`
-	Address      *string        `json:"address"`
-	Timezone     *string        `json:"timezone"`
+	FirstName     *string        `json:"first_name"`
+	LastName      *string        `json:"last_name"`
+	Email         string         `json:"email" binding:"required"`
+	AvatarURL     *string        `json:"avatar_url"`
+	SignatureURL  *string        `json:"signature_url"`
+	Country       *model.Country `json:"country"`
+	Phone         *string        `json:"phone"`
+	Address       *string        `json:"address"`
+	Timezone      *string        `json:"timezone"`
+	LicenseNumber *string        `json:"license_number"`
 }
